ad528444b2c87b2e7a525324b45ca811fb610df9
Puhing <33rnzla@gmail.com>
2024-02-18 04:17:20 +0900

    chat function finished

--- routes/chat.ts
+++ routes/chat.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import Server from 'socket.io';
-import { Socket } from 'socket.io';
 import { createServer } from 'http';
 import mysql from 'mysql2';
 import bodyParser from 'body-parser';
 import { upload } from '../util/fileupload';
 import fs from 'fs';
 import path from 'path';
+// import { MIMEType } from 'util';
+// import { Socket } from 'socket.io';
 
 const app = express();
 const router = express.Router();
@@ -31,6 +32,8 @@ const hours = String(currentTime.getHours()).padStart(2, '0');
 const minutes = String(currentTime.getMinutes()).padStart(2, '0');
 const seconds = String(currentTime.getSeconds()).padStart(2, '0');
 const MonthDayTime = `${month}_${day}_${hours}:${minutes}:${seconds}`;
+const fileName = `Message_${MonthDayTime}.txt`;
+const filePath = path.join(__dirname, '../public/uploads', fileName);
 
 var roomList = ['public'];
 var roomNow = '';
@@ -47,13 +50,10 @@ router.post('/save_chat', async (req, res) => {
         const receivedData = req.body;
         const { socketId, content, nickname, roomNow } = req.body;
         
-        const fileName = `Message_${MonthDayTime}.txt`;
-        const filePath = path.join(__dirname, '../public/uploads', fileName);
-        
         fs.writeFileSync(filePath, content, 'utf-8');
 
-        console.log('파일이 저장되었습니다:', filePath);
         console.log('데이터 받은거', receivedData);
+        console.log('파일이 저장되었습니다:', filePath);
 
         const result = await connection.execute('INSERT INTO TB_USER_CHAT (socketId, content, type, addedAt, nickname, roomNow) VALUES (?,?,?,?,?,?)', [
             socketId,
@@ -64,12 +64,43 @@ router.post('/save_chat', async (req, res) => {
             roomNow,
         ]);
         connection.end();
-        res.status(200).json({ message: 'Data saved successfully' });
+        res.status(200).json({ message: 'Message data saved successfully' });
     } catch (err) {
         console.log('err:', err);
         res.status(500).json({ message: 'Server Error' });
     }
 });
+
+router.post('/save_file', upload.single('file'), async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const receivedData = req.body;
+        const receivedFile = req.file;
+        const { socketId, file, nickname, roomNow } = req.body;
+        console.log('바디',receivedData,'파일',receivedFile, '마임타입',receivedFile.mimetype);
+        let type = '';
+        
+        if(receivedFile.mimetype === 'image/png'){
+            type = '3';
+        } else {
+            type = '2';
+        }
+
+        const result =  await connection.execute('INSERT INTO TB_USER_CHAT (socketId, content, type, addedAt, nickname, roomNow) VALUES (?,?,?,?,?,?)', [
+            socketId || null,
+            receivedFile.path || null,
+            type || null,
+            currentTime || null,
+            nickname || null,
+            roomNow || null,
+        ]);
+        connection.end();
+        res.status(200).json({ message: 'File data saved successfully' });
+    } catch (err) {
+        console.log('err:', err);
+        res.status(500).json({ message: 'Server Error' });
+    }
+})
 
 io.on('connection', socket => {
     socket.join(publicRoom);
--- util/fileupload.ts
+++ util/fileupload.ts
@@ -1,20 +1,31 @@
 import multer from "multer";
 
+const currentTime = new Date();
+const month = String(currentTime.getMonth() + 1).padStart(2, '0');
+const day = String(currentTime.getDate()).padStart(2, '0');
+const hours = String(currentTime.getHours()).padStart(2, '0');
+const minutes = String(currentTime.getMinutes()).padStart(2, '0');
+const seconds = String(currentTime.getSeconds()).padStart(2, '0');
+const MonthDayTime = `${month}_${day}_${hours}:${minutes}:${seconds}`;
+const fileName = `Message_${MonthDayTime}.txt`;
+
 let storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, "public/uploads/");
     },
     filename: function (req, file, callback) {
+        const utf8FileName = Buffer.from(file.originalname, 'binary').toString('utf-8');
+
         if (file.mimetype === 'image/png'){
-            callback(null, Date.now() + "img_" + file.originalname + ".jpeg");
+            callback(null, "img_" + fileName + utf8FileName + ".jpeg");
         } else {
-            callback(null, Date.now() + "_" + file.originalname + ".webm");
+            callback(null, "audio_" + fileName + utf8FileName + ".webm");
         }
     },
 });
 
 const limits = {
-    filedSize: 1024 * 1024,
+    filedSize: 1024 * 1024, 
     fileSize: 1024 * 1024 * 20, // 20mb
     files: 10,
 };
